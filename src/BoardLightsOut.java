6b72609f3ae3ca07e29cc0f0313af672f9299d1b
Tomago01 <tom.bur.x123@gmail.com>
2024-05-06 22:06:11 +0200

    pridani kostruktoru do tridy BoardLightsOut

---
+++ src/BoardLightsOut.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class BoardLightsOut extends JFrame {
+        private int[][] board;
+        private int size;
+        private JButton[][] buttons;
+
+    public BoardLightsOut(int size) {
+        this.size = size;
+        this.board = new int[size][size];
+        this.buttons = new JButton[size][size];
+
+        }
+
+
+
+
+
+
+}
