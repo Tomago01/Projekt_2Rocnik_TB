96d030075a3cbcf1d60659e90773906ebd0d302d
Tomago01 <tom.bur.x123@gmail.com>
2024-05-09 20:38:16 +0200

    pridani nove tridy LogicButtonsClick ve ktere budu delat logiku reagovani tlacitek ve hre lightsout

---
+++ src/LogicButtonsClick.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class LogicButtonsClick {
+        private int row;
+        private int column;
+        private int[][] board;
+        private JButton[][] buttons;
+        private int size;
+
+        public LogicButtonsClick(int row, int column, int[][] board, JButton[][] buttons, int size) {
+            this.row = row;
+            this.column = column;
+            this.board = board;
+            this.buttons = buttons;
+            this.size = size;
+        }
+
+
+
+
+
+}
