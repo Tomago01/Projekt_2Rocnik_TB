eecc502f946459e031e99cf57ed850e537bd214a
Tomago01 <tom.bur.x123@gmail.com>
2024-05-29 20:10:47 +0200

    pridal jsem metodu initializeWorld ve ktere definuji charaktery pro hrace zavinacem a targetsForBoxes teckou. Metoda prochazi tu delku X,Y toho levlu a na tyhle charaktery nastavi danou funkci

--- src/Sokoban/BoardSokoban.java
+++ src/Sokoban/BoardSokoban.java
@@ -6,13 +6,31 @@ public class BoardSokoban{
 
     private int playerX, playerY;
 
+    private boolean[][] targetsForBoxes;
+
     private char[][] currentLevel;
     private int currentLevelNumber;
 
     public BoardSokoban(char[][] level, int levelNumber) {
         currentLevelNumber = levelNumber;
         currentLevel = this.level;
+        this.targetsForBoxes = new boolean[level.length][level[0].length];
     }
+
+    public void initializeWorld() {
+        for (int y = 0; y < level.length; y++) {
+            for (int x = 0; x < level[y].length; x++) {
+                char item = level[y][x];
+                if (item == '@') {
+                    playerX = x;
+                    playerY = y;
+                } else if (item == '.') {
+                    targetsForBoxes[y][x] = true;
+                }
+            }
+        }
+    }
+
 
 
 }
