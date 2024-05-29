d9c07ed5f64b9fe3d3cd3fadcab09f412b8a8d67
Tomago01 <tom.bur.x123@gmail.com>
2024-05-29 21:20:07 +0200

    Vytvoreni metody drawWorld ktera vykresluje jak na herni desce budou vypadat jednotlive znaky pomoci novych promenych space a offset, offset jevelikost v pixelech od okraje plochy na ktere jsou charaktery a space jsou jednotliva policka. Trida Graphics mi pomaha pridat Images na znaky tyto obrazky jsem dal do slozky sources.

--- src/Sokoban/BoardSokoban.java
+++ src/Sokoban/BoardSokoban.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class BoardSokoban extends JPanel {
 
+    private final int offset = 100;
+    private final int space = 80;
     private final int width = 1600;
     private final int height = 1600;
 
@@ -21,6 +23,7 @@ public class BoardSokoban extends JPanel {
         currentLevelNumber = levelNumber;
         currentLevel = this.level;
         this.targetsForBoxes = new boolean[level.length][level[0].length];
+        initializeBoard();
     }
 
     private void initializeBoard() {
@@ -46,6 +49,37 @@ public class BoardSokoban extends JPanel {
         }
     }
 
+    private void drawWorld(Graphics g) {
+        ImageIcon imageIcon1 = new ImageIcon("sources/woodenbox.png");
+        ImageIcon imageIcon2 = new ImageIcon("sources/wall.png");
+        ImageIcon imageIcon3 = new ImageIcon("sources/grass.png");
+        ImageIcon imageIcon4 = new ImageIcon("sources/placespot.png");
+        Image image1 = imageIcon1.getImage();
+        Image image2 = imageIcon2.getImage();
+        Image image3 = imageIcon3.getImage();
+        Image image4 = imageIcon4.getImage();
 
+        for (int y = 0; y < level.length; y++) {
+            for (int x = 0; x < level[y].length; x++) {
+                char character = level[y][x];
+                boolean isTargetsForBoxes = targetsForBoxes[y][x];
+
+                g.drawImage(image3, x * space + offset, y * space + offset, space, space, null);
+
+                if (isTargetsForBoxes) {
+                    g.drawImage(image4, x * space + offset, y * space + offset, space, space, null);
+                }
+
+                if (character == '#') {
+                    g.drawImage(image2, x * space + offset, y * space + offset, space, space, null);
+                } else if (character == '$') {
+                    g.drawImage(image1, x * space + offset, y * space + offset, space, space, null);
+                } else if (character == '@') {
+                    g.setColor(new Color(0x017C83));
+                    g.fillOval(x * space + offset, y * space + offset, space, space);
+                }
+            }
+        }
+    }
 
 }
